3d12b66ea397ccd5988255905ceacd74b0cfc825
Andrea Riley <andreariley@me.com>
2024-03-09 15:59:29 -0500

    added decorators

--- decorators.py
+++ decorators.py
@@ -1,5 +1,6 @@
 from functools import wraps
 from time import sleep
+import logging
 
 
 def procrastinate(func):
@@ -10,3 +11,13 @@ def procrastinate(func):
 
     return wrapper
 
+def logging(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        logger = logging.getLogger(__name__)
+        logger.info(f'Calling function {func.__name__} with args: {args}, kwargs: {kwargs}')
+        result = func(*args, **kwargs)
+        logger.info(f'Calling function {func.__name__} returned: {result}')
+        return result
+
+    return wrapper
